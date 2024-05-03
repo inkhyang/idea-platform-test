9a42c4f85f6dd8fb048cdf56d3a6447991a9e836
inkhyang <adeqvatn@gmail.com>
2024-05-03 20:23:40 +0400

    init

--- src/test/java/com/inkhyang/service/FlightServiceTest.java
+++ src/test/java/com/inkhyang/service/FlightServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.List;
 
-import static TicketsParser.jsonParser;
+import static com.inkhyang.utils.TicketsParser.jsonParser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlightServiceTest {
